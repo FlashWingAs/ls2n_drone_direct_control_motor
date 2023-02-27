c141de0cbfc36aeca3ceee9b774e03ee23898332
Julien Stauder <julien.stauder@eleves.ec-nantes.fr>
2023-02-27 17:16:32 +0100

    Feat: add ControlCenter base framework

--- ls2n_drone_direct_motor_control/direct_motor_control.py
+++ ls2n_drone_direct_motor_control/direct_motor_control.py
@@ -1,5 +1,131 @@
-def main():
-    print('Hi from ls2n_drone_direct_motor_control.')
+import importlib
+import numpy as np
+import os
+import subprocess
+import signal
+import rclpy
+from geometry_msgs.msg import Vector3Stamped
+from ls2n_interfaces.msg import (
+    AttitudeThrustSetPoint,
+    DroneStatus,
+    RatesThrustSetPoint,
+    KeepAlive,
+    MotorControlSetPoint,
+)
+from ls2n_interfaces.srv import DroneRequest, OnboardApp
+from nav_msgs.msg import Odometry
+from trajectory_msgs.msg import JointTrajectory
+from rcl_interfaces.msg import ParameterDescriptor, ParameterType
+from rclpy.node import Node
+from rclpy.parameter import Parameter
+
+from ls2n_drone_bridge.common import (
+    DroneRequestString,
+    DroneStatusString,
+    FullState,
+    qos_profile_sensor_data,
+)
+from ls2n_drone_bridge.controllers import (
+    AccelerationController,
+    AttitudeThrustController,
+    Controller,
+    ControllerType,
+    PositionController,
+    RatesThrustController,
+    VelocityController,
+    MotorController,
+)
+from ls2n_drone_bridge.drone_bridge import (
+    Status
+)
+
+class ControlCenter(Node):
+    def __init__(self):
+        super().__init__("direct_motor_control")
+        parameters = [
+            ["mass", 1.0, ParameterType.PARAMETER_DOUBLE, "Drone mass"],
+            [
+                "take_off_height",
+                1.0,
+                ParameterType.PARAMETER_DOUBLE,
+                "Height for automatic take off",
+            ],
+        ]
+        for parameter in parameters:
+            self.declare_parameter(
+                parameter[0],
+                parameter[1],
+                ParameterDescriptor(
+                    type=parameter[2], description=parameter[3]
+                ),
+            )
+            setattr(
+                self,
+                parameter[0],
+                lambda param=parameter[0]: self.get_parameter(param).value,
+            )
+
+        self.get_logger().info("Starting control center node")
+        
+        # Bridge feedback
+        self.create_subscription(
+            DroneStatus,
+            "Status",
+            self.status_update_callback,
+            qos_profile_sensor_data,
+        )
+        self.create_subscription(
+            Odometry,
+            "EKF/odom",
+            self.odom_update_callback,
+            qos_profile_sensor_data,
+        )
+        self.status_timer = self.create_timer(0.01, self.publish_status)
+        # MOCAP subscription
+        # Set points
+        self.direct_motor_control_publisher = self.create_publisher(
+            MotorControlSetPoint, "MotorcontrolSetPoint", qos_profile_sensor_data
+        )
+
+        self.create_subscription(
+            JointTrajectory,
+            "Trajectory",
+            self.trajectory_callback,
+            qos_profile_sensor_data,
+        )
+        
+        # subscribers related to disturbances observation
+        self.create_subscription(
+            Vector3Stamped,
+            "Observer/DisturbancesWorld",
+            self.disturbances_callback,
+            qos_profile_sensor_data,
+        )
+        self.main_loop_timer = self.create_timer(
+            0.01, self.main_loop
+            )
+        self.recovery_timer = self.create_timer(
+            10.0, self.recover_from_emergency
+        )
+        self.recovery_timer.cancel()
+        self.status.lastAlive = self.get_clock().now()
+
+    status = Status()
+  
+    def status_update_callback(self,rcvd_msg):
+        self.status.status = rcvd_msg.status
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ControlCenter()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print("Shutting down drone bridge")
+    finally:
+        node.destroy_node()
+        rclpy.try_shutdown()
 
 
 if __name__ == '__main__':
