8ca934f0949be88b0bb22b4ccf86fde5b64e06c0
Julien Stauder <julien.stauder@eleves.ec-nantes.fr>
2023-05-03 17:44:11 +0200

    Feat: Add YAML config file access

--- launch/direct_motor_control.launch.py
+++ launch/direct_motor_control.launch.py
@@ -2,6 +2,11 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from nav2_common.launch import RewrittenYaml
+from ament_index_python.packages import get_package_share_directory
+import os
+
+model = "tilthex"
 
 def generate_launch_description():
     return LaunchDescription(
@@ -16,6 +21,15 @@ def generate_launch_description():
                 executable="direct_motor_control",
                 output="screen",
                 namespace=LaunchConfiguration("drone_namespace"),
+                parameters = [RewrittenYaml(
+                    source_file=os.path.join(
+                    get_package_share_directory("ls2n_drone_sitl"),
+                    "config",
+                    "sitl_" + model + "_params.yaml",
+                ),
+                root_key="Drone1",
+                param_rewrites={},
+                )]
             )
         ]
     )
